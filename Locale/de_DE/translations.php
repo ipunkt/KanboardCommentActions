61fdee0d4454f1e1de43af6579c7072868f1a01b
andrei <av@ipunkt.biz>
2019-10-14 13:13:30 +0200

    translation for settings page removed

--- Locale/de_DE/translations.php
+++ Locale/de_DE/translations.php
@@ -4,15 +4,8 @@ return array(
     'Choose user' => 'Nutzer auswählen',
     'Comment added successfully.' => 'Kommentar erfolgreich hinzugefügt.',
     'Unable to create your comment.' => 'Kommentar kann nicht erstellt werden.',
-    'Settings saved successfully.' => 'Einstellungen erfolgreich gespeichert.',
-    'Unable to save your settings.' => 'Die Einstellungen können nicht gespeichert werden.',
-    'Enable "Comment Actions"' => '"Kommentaraktionen" aktivieren',
-    'Save' => 'Speichern',
     'Add a comment' => 'Kommentar hinzufügen',
     'Edit a comment' => 'Kommentar bearbeiten',
-    'Settings' => 'Einstellungen',
-    'Comment Actions' => 'Kommentaraktionen',
-    'Comment Actions settings' => 'Einstellungen für Kommentaraktionen',
     'This plugin gives the ability to assign actual Task (direct in comment-create section) to one of the users in Project.'
     => 'Dieses Plugin bietet die Möglichkeit, einem Benutzer im Projekt eine aktuelle Aufgabe (direkt im Abschnitt zum Erstellen von Kommentaren) zuzuweisen.',
 );
--- Locale/ru_RU/translations.php
+++ Locale/ru_RU/translations.php
@@ -4,16 +4,8 @@ return array(
     'Choose user' => 'Выберете пользователя',
     'Comment added successfully.' => 'Комментарий успешно добавлен.',
     'Unable to create your comment.' => 'Невозможно добавить Ваш комментарий.',
-    'Settings saved successfully.' => 'Настройки успешно сохранены.',
-    'Unable to save your settings.' => 'Невозможно сохранить Ваши настройки.',
-    'Enable "Comment Actions"' => 'Активировать "Comment Actions"',
-    'Save' => 'Сохранить',
     'Add a comment' => 'Добавить комментарий',
     'Edit a comment' => 'Редактировать комментарий',
-    'Settings' => 'Настройки',
-    'Comment Actions' => 'Comment Actions',
-    'Comment Actions settings' => 'Настройки "Comment Actions"',
     'This plugin gives the ability to assign actual Task (direct in comment-create section) to one of the users in Project.'
     => 'Этот плагин даёт возможность (при написании комментария) назначить текущую задачу пользователю из текущего проекта.'
-
 );
