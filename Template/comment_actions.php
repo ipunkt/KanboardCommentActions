fa1bc519288e4fc8d7441df42f9bf60d2f9c7bad
andrei <av@ipunkt.biz>
2019-10-14 13:14:03 +0200

    styling for dropdown list reworked

--- Template/comment_actions.php
+++ Template/comment_actions.php
@@ -1,6 +1,8 @@
-<h4><?= t('Assign Task to:') ?></h4>
-<?= $this->app->component('select-dropdown-autocomplete', array(
-    'name' => 'user_id',
-    'items' => $users_list,
-    'placeholder' => t('Choose user')
-)) ?>
+<div class="input-addon margin-top markdown">
+    <p class="pagination-previous"><?= t('Assign Task to:') ?></p>
+    <?= $this->app->component('select-dropdown-autocomplete', array(
+        'name' => 'user_id',
+        'items' => $users_list,
+        'placeholder' => t('Choose user')
+    )) ?>
+</div>
